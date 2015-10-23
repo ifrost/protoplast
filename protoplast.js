9050460d335be87ecc1d1830dcdd5491a3b5f331
Piotr Jamroz <pm.jamroz@gmail.com>
2015-10-23 21:03:06 +0100

    Add simple inheritance support

--- protoplast.js
+++ protoplast.js
@@ -1,6 +1,21 @@
 (function(exports) {
     "use strict";
 
-    exports.Proto = "Proto";
+    var Proto = {};
+    Proto.extend = function(factory) {
+        var proto = Object.create(this), constructor;
+        factory(proto, this);
+
+        constructor = function() {
+            var instance = Object.create(proto);
+            instance.init.apply(instance, arguments);
+            return instance;
+        };
+        constructor.extend = Proto.extend.bind(proto);
+        return constructor;
+    };
+    Proto.init = function() {};
+
+    exports.Proto = Proto;
 
 })(this);
--- test/protoplastTest.js
+++ test/protoplastTest.js
@@ -1,7 +1,54 @@
 describe('Protoplast', function(){
 
-    it('basic test', function() {
-        chai.assert.equal(Proto, "Proto");
+    it('creates a simple prototype', function() {
+
+        var Base, base;
+
+        Base = Proto.extend(function(proto){
+            proto.value = 10;
+        });
+
+        base = Base();
+
+        chai.assert.equal(base.value, 10);
     });
+
+    it('runs init method with passed arguments when object is created', function() {
+
+        var Base, base;
+
+        Base = Proto.extend(function(proto){
+            proto.init = function(value) {
+                this.value = value;
+            }
+        });
+
+        base = Base(10);
+
+        chai.assert.equal(base.value, 10);
+    });
+
+    it('allows to run base methods', function() {
+
+        var Base, Sub, base, sub;
+
+        Base = Proto.extend(function(proto){
+            proto.test = function() {
+                return 10;
+            }
+        });
+
+        Sub = Base.extend(function(proto, $super){
+            proto.test = function() {
+                return $super.test.call(this) * 2;
+            }
+        });
+
+        base = Base();
+        sub = Sub();
+
+        chai.assert.equal(base.test(), 10);
+        chai.assert.equal(sub.test(), 20);
+    })
 
 });
