e33e20c4ab20b592b6cdceeff418dcf400db220b
Piotr Jamroz <pm.jamroz@gmail.com>
2015-10-23 22:53:40 +0100

    Flux architecture example

--- test/protoplastTest.js
+++ test/protoplastTest.js
@@ -120,7 +120,7 @@ describe('Protoplast', function(){
             }
         });
 
-        UCText = Text.extend(function(proto, base){
+        UCText = Text.extend(function(proto){
             proto.toUpperCase = function() {
                 this.text = this.text.toUpperCase();
             }
@@ -186,7 +186,7 @@ describe('Protoplast', function(){
 
     });
 
-    it.only('dispatches messages between objects', function(){
+    it('dispatches messages between objects', function(){
 
         var Source, Destination, source, destination;
 
@@ -222,6 +222,93 @@ describe('Protoplast', function(){
         destination.clear();
         source.send('awww');
         chai.assert.equal(destination.message, 'hi');
+
+    });
+
+    it('flux example', function() {
+
+        var RootView, View, ActionDispatcher, Repository, view, repository;
+
+        // create root view that will be injected into repositories
+        RootView = Proto.extend(function(proto) {
+            proto.init = function() {
+                this.view = View();
+            };
+            proto.data = function(data) {
+                this.view.show(data.clicks);
+            };
+        });
+
+        // create a clickable component that displays number of clicks
+        View = Proto.extend(function(proto, $super, config){
+            config.pub = 'pub';
+
+            proto.show = function(value) {
+               this.value = value;
+            };
+
+            proto.click = function() {
+                this.pub('myview/clicked');
+            }
+        });
+
+        // action dispatcher to convert view actions to domain actions
+        ActionDispatcher = Proto.extend(function(proto, $super, config){
+            config.sub = 'sub';
+            config.pub = 'pub';
+
+            proto.init = function() {
+                this.sub('myview/clicked').add(this.count_click);
+            };
+
+            proto.count_click = function() {
+                this.pub('click/increment', 1);
+            };
+        });
+
+        // repository to react to domain actions and pass data to the view
+        Repository = Proto.extend(function(proto, $super, config){
+            config.sub = 'sub';
+            config.view = 'view';
+
+            proto.init = function() {
+                this.clicks = 0;
+                this.sub('click/increment').add(this.increment);
+                this.refresh();
+            };
+
+            proto.increment = function(value) {
+                this.clicks += value;
+                this.refresh();
+            };
+
+            proto.refresh = function() {
+                this.view().data({clicks: this.clicks});
+            }
+
+        });
+
+        // capture view for testing
+        var _view = null;
+        View.aop('init', {
+            after: function() {
+                _view = this;
+            }
+        });
+
+        Proto.register('view', RootView());
+        ActionDispatcher();
+        repository = Repository();
+
+        chai.assert.equal(repository.clicks, 0);
+        chai.assert.equal(_view.value, 0);
+
+        _view.click();
+        _view.click();
+        _view.click();
+
+        chai.assert.equal(repository.clicks, 3);
+        chai.assert.equal(_view.value, 3);
 
     });
 
