6ff349404c38a92f8715ab746953adf2dc8d8df6
Travis CI <travis@travis-ci.org>
2017-11-30 23:31:43 +0000

    v2.0.4

--- dist/protoplast.js
+++ dist/protoplast.js
@@ -7,7 +7,7 @@
 		exports["Protoplast"] = factory();
 	else
 		root["Protoplast"] = factory();
-})(this, function() {
+})(typeof self !== 'undefined' ? self : this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
